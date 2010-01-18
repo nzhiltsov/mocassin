c474a0abcac88f8937efebb40d05058cd09be06d
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2010-01-18 14:50:23 +0000

    load concepts elements for concept query node [Input 11]

--- trunk/omdoc-search-engine-ui/src/ru/ksu/niimm/ose/ui/client/widget/suggestbox/OntologyElementSuggestBox.java
+++ trunk/omdoc-search-engine-ui/src/ru/ksu/niimm/ose/ui/client/widget/suggestbox/OntologyElementSuggestBox.java
@@ -2,12 +2,12 @@ package ru.ksu.niimm.ose.ui.client.widget.suggestbox;
 
 import com.google.gwt.event.dom.client.FocusEvent;
 import com.google.gwt.event.dom.client.FocusHandler;
+import com.google.gwt.uibinder.client.UiConstructor;
 import com.google.gwt.user.client.ui.SuggestBox;
 
-public class OntologyElementSuggestBox2 extends SuggestBox implements
+public class OntologyElementSuggestBox extends SuggestBox implements
 		FocusHandler {
-
-	public OntologyElementSuggestBox2(OntologyElementOracle oracle) {
+	public OntologyElementSuggestBox(OntologyElementOracle oracle) {
 		super(oracle);
 		getTextBox().addFocusHandler(this);
 	}
@@ -15,6 +15,10 @@ public class OntologyElementSuggestBox2 extends SuggestBox implements
 	@Override
 	public void onFocus(FocusEvent event) {
 		showSuggestionList();
+	}
+
+	public OntologyElementOracle getOracle() {
+		return (OntologyElementOracle) getSuggestOracle();
 	}
 
 }
