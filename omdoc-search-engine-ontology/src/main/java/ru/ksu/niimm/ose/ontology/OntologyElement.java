97015f8fd5618070e1a14d51748618472b201ede
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2010-01-24 18:03:12 +0000

    fixed the bug with subclasses of range of given property [Issue 25]

--- omdoc-search-engine-ontology/src/main/java/ru/ksu/niimm/ose/ontology/OntologyElement.java
+++ omdoc-search-engine-ontology/src/main/java/ru/ksu/niimm/ose/ontology/OntologyElement.java
@@ -45,4 +45,29 @@ public class OntologyElement {
 		return label;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((uri == null) ? 0 : uri.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OntologyElement other = (OntologyElement) obj;
+		if (uri == null) {
+			if (other.uri != null)
+				return false;
+		} else if (!uri.equals(other.uri))
+			return false;
+		return true;
+	}
+
 }
