be09d280f071ccdd11b8dc6d52f832a35f3baed0
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2010-04-05 19:37:38 +0000

    added DAO insert triples method implementation [Issue 31]

--- trunk/mocassin-virtuoso/src/main/java/ru/ksu/niimm/cll/mocassin/virtuoso/impl/RDFGraphImpl.java
+++ trunk/mocassin-virtuoso/src/main/java/ru/ksu/niimm/cll/mocassin/virtuoso/impl/RDFGraphImpl.java
@@ -3,18 +3,63 @@ package ru.ksu.niimm.cll.mocassin.virtuoso.impl;
 import ru.ksu.niimm.cll.mocassin.virtuoso.RDFGraph;
 
 public class RDFGraphImpl implements RDFGraph {
-	private String uri;
+	private final String username;
+	private final String password;
+	private final String iri;
+	private final String url;
 
-	public RDFGraphImpl(String uri) {
-		this.uri = uri;
+	public static class Builder {
+		private final String iri;
+
+		private String username;
+		private String password;
+		private String url;
+
+		public Builder(String iri) {
+			this.iri = iri;
+		}
+
+		public Builder username(String username) {
+			this.username = username;
+			return this;
+		}
+
+		public Builder password(String password) {
+			this.password = password;
+			return this;
+		}
+
+		public Builder url(String url) {
+			this.url = url;
+			return this;
+		}
+
+		public RDFGraph build() {
+			return new RDFGraphImpl(this);
+		}
 	}
 
-	public String getUri() {
-		return uri;
+	private RDFGraphImpl(Builder builder) {
+		this.iri = builder.iri;
+		this.username = builder.username;
+		this.password = builder.password;
+		this.url = builder.url;
 	}
 
-	public void setUri(String uri) {
-		this.uri = uri;
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getIri() {
+		return iri;
+	}
+
+	public String getUrl() {
+		return url;
 	}
 
 }
