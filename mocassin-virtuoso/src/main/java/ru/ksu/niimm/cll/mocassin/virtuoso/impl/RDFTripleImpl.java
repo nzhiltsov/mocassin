c2a9f33658d3998be40a101e44a05393e3efc36f
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2010-04-07 14:51:29 +0000

    fixed test by configuring Virtuoso graph and user permissions [Issue 31]

--- mocassin-virtuoso/src/main/java/ru/ksu/niimm/cll/mocassin/virtuoso/impl/RDFTripleImpl.java
+++ mocassin-virtuoso/src/main/java/ru/ksu/niimm/cll/mocassin/virtuoso/impl/RDFTripleImpl.java
@@ -1,41 +1,20 @@
 package ru.ksu.niimm.cll.mocassin.virtuoso.impl;
 
-import ru.ksu.niimm.cll.mocassin.virtuoso.RDFNode;
 import ru.ksu.niimm.cll.mocassin.virtuoso.RDFTriple;
 
 public class RDFTripleImpl implements RDFTriple {
-	private RDFNode subject;
-	private RDFNode predicate;
-	private RDFNode object;
+	private String value;
 
-	public RDFTripleImpl(RDFNode subject, RDFNode predicate, RDFNode object) {
-		this.subject = subject;
-		this.predicate = predicate;
-		this.object = object;
+	public RDFTripleImpl(String value) {
+		this.value = value;
 	}
 
-	public RDFNode getSubject() {
-		return subject;
+	public String getValue() {
+		return value;
 	}
 
-	public void setSubject(RDFNode subject) {
-		this.subject = subject;
-	}
-
-	public RDFNode getPredicate() {
-		return predicate;
-	}
-
-	public void setPredicate(RDFNode predicate) {
-		this.predicate = predicate;
-	}
-
-	public RDFNode getObject() {
-		return object;
-	}
-
-	public void setObject(RDFNode object) {
-		this.object = object;
+	public void setValue(String value) {
+		this.value = value;
 	}
 
 }
--- mocassin-virtuoso/src/main/java/ru/ksu/niimm/cll/mocassin/virtuoso/query/QueryBuilder.java
+++ mocassin-virtuoso/src/main/java/ru/ksu/niimm/cll/mocassin/virtuoso/query/QueryBuilder.java
@@ -2,11 +2,8 @@ package ru.ksu.niimm.cll.mocassin.virtuoso.query;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.regex.Pattern;
 
-import ru.ksu.niimm.cll.mocassin.virtuoso.RDFNode;
 import ru.ksu.niimm.cll.mocassin.virtuoso.RDFTriple;
-import ru.ksu.niimm.cll.mocassin.virtuoso.RDFNode.Type;
 
 /**
  * SPARQL query builder according to SPARQL/Update grammar
@@ -83,9 +80,7 @@ public class QueryBuilder {
 	private String buildInsertQuery() {
 		StringBuffer constructTemplate = new StringBuffer();
 		for (RDFTriple triple : getTriples()) {
-			String tripleStr = String.format("%s %s %s .", triple.getSubject()
-					.getUri(), triple.getPredicate().getUri(), triple
-					.getObject().getUri());
+			String tripleStr = String.format("%s .", triple.getValue());
 			constructTemplate.append(tripleStr);
 		}
 		return String.format(INSERT_EXPRESSION, getGraphUri(),
@@ -106,63 +101,12 @@ public class QueryBuilder {
 		return !isEmpty(getGraphUri());
 	}
 
-	private boolean validateNumeric(String str) {
-		return Pattern.matches("[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?$", str);
-	}
-
 	private boolean isEmpty(String str) {
 		return str == null || str.length() == 0;
 	}
 
-	private boolean validateNode(RDFNode node) {
-		if (node == null)
-			return false;
-
-		String nodeUri = node.getUri();
-		Type nodeType = node.getType();
-		if (isEmpty(nodeUri) || nodeType == null) {
-			return false;
-		}
-		switch (nodeType) {
-		case IRI_REFERENCE:
-			return validateIRIReference(nodeUri);
-		case RDF_LITERAL:
-			return validateRDFLiteral(nodeUri);
-		case NUMERIC_LITERAL:
-			return validateNumeric(nodeUri);
-		case BOOLEAN_LITERAL:
-			return validateBoolean(nodeUri);
-		case BLANK_NODE:
-			return validateBlankNode(nodeUri);
-		default:
-			throw new IllegalArgumentException(String.format(
-					"this type of RDF node isn't supported: %s", nodeType));
-		}
-	}
-
-	private boolean validateIRIReference(String nodeUri) {
-		// TODO : complete implementation
-		return true;
-	}
-
-	private boolean validateBlankNode(String nodeUri) {
-		// TODO :complete implementation
-		return nodeUri.startsWith("_:");
-	}
-
-	private boolean validateRDFLiteral(String nodeUri) {
-		// TODO complete implementation
-		return true;
-	}
-
-	private boolean validateBoolean(String nodeUri) {
-		return nodeUri.equals("true") || nodeUri.equals("false");
-	}
-
 	private boolean validateTriple(RDFTriple triple) {
-		return validateNode(triple.getSubject())
-				&& validateNode(triple.getPredicate())
-				&& validateNode(triple.getObject());
+		return !isEmpty(triple.getValue());
 	}
 
 	private boolean validateInsert() {
