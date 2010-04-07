40c34a1e8c81edb1eefd8e10a5b2543784e457e4
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2010-04-07 14:51:29 +0000

    fixed test by configuring Virtuoso graph and user permissions [Issue 31]

--- trunk/mocassin-virtuoso/src/main/java/ru/ksu/niimm/cll/mocassin/virtuoso/RDFTriple.java
+++ trunk/mocassin-virtuoso/src/main/java/ru/ksu/niimm/cll/mocassin/virtuoso/RDFTriple.java
@@ -1,9 +1,21 @@
 package ru.ksu.niimm.cll.mocassin.virtuoso;
 
 public interface RDFTriple {
-	RDFNode getSubject();
-
-	RDFNode getPredicate();
-
-	RDFNode getObject();
+	/**
+	 * 
+	 * return RDF triple representation in the N3 format, e.g.
+	 * <p>
+	 * <ul>
+	 * <li>
+	 * 
+	 * <pre>
+	 * < all.omdoc#whatislogic >	< http://www.w3.org/1999/02/22-rdf-syntax-ns#type >	< http://omdoc.org/ontology#Theory >
+	 * </pre>
+	 * 
+	 * </code>
+	 * </ul>
+	 * 
+	 * @return RDF triple representation in the N3 format
+	 */
+	String getValue();
 }
--- trunk/mocassin-virtuoso/src/test/java/unittest/InsertQueryGeneratorTest.java
+++ trunk/mocassin-virtuoso/src/test/java/unittest/InsertQueryGeneratorTest.java
@@ -3,13 +3,15 @@ package unittest;
 import java.util.ArrayList;
 import java.util.List;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import ru.ksu.niimm.cll.mocassin.virtuoso.RDFGraph;
 import ru.ksu.niimm.cll.mocassin.virtuoso.RDFTriple;
-import ru.ksu.niimm.cll.mocassin.virtuoso.VirtuosoDAO;
 import ru.ksu.niimm.cll.mocassin.virtuoso.VirtuosoModule;
+import ru.ksu.niimm.cll.mocassin.virtuoso.generator.InsertQueryGenerator;
 import ru.ksu.niimm.cll.mocassin.virtuoso.impl.RDFGraphImpl;
 import ru.ksu.niimm.cll.mocassin.virtuoso.impl.RDFTripleImpl;
 
@@ -19,29 +21,35 @@ import com.mycila.testing.plugin.guice.GuiceContext;
 
 @RunWith(MycilaJunitRunner.class)
 @GuiceContext(VirtuosoModule.class)
-public class VirtuosoDAOTest {
+public class InsertQueryGeneratorTest {
+	private static final String TRIPLE_2 = "<all.omdoc#whatislogic> <http://salt.semanticauthoring.org/onto/abstract-document-ontology#hasPart> <all.omdoc#whatislogic.p11>";
+	private static final String TRIPLE_1 = "<all.omdoc#whatislogic> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://omdoc.org/ontology#Theory>";
 	private static final String MOCASSIN_TEST_URL = "jdbc:virtuoso://192.168.2.143:1111";
 	private static final String MOCASSIN_TEST_PASSWORD = "mocassintest";
 	private static final String MOCASSIN_TEST_USERNAME = "mocassinuser";
 	private static final String MOCASSIN_TEST_GRAPH_IRI = "<http://cll.niimm.ksu.ru/mocassintest>";
+
 	@Inject
-	private VirtuosoDAO virtuosoDAO;
+	private InsertQueryGenerator insertQueryGenerator;
 
 	@Test
-	public void testInsert() {
+	public void testGenerate() {
 		List<RDFTriple> triples = new ArrayList<RDFTriple>();
-		RDFTriple triple = new RDFTripleImpl(
-				"<all.omdoc#whatislogic> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://omdoc.org/ontology#Theory>");
+		RDFTriple triple = new RDFTripleImpl(TRIPLE_1);
+		RDFTriple triple2 = new RDFTripleImpl(TRIPLE_2);
 		triples.add(triple);
+		triples.add(triple2);
 		RDFGraph graph = new RDFGraphImpl.Builder(MOCASSIN_TEST_GRAPH_IRI)
 				.username(MOCASSIN_TEST_USERNAME).password(
 						MOCASSIN_TEST_PASSWORD).url(MOCASSIN_TEST_URL).build();
-
-		getVirtuosoDAO().insert(triples, graph);
+		String expression = getInsertQueryGenerator().generate(triples, graph);
+		Assert.assertTrue(expression.equalsIgnoreCase(String.format(
+				"INSERT INTO GRAPH %s {%s .%s .}", MOCASSIN_TEST_GRAPH_IRI,
+				TRIPLE_1, TRIPLE_2)));
 	}
 
-	public VirtuosoDAO getVirtuosoDAO() {
-		return virtuosoDAO;
+	public InsertQueryGenerator getInsertQueryGenerator() {
+		return insertQueryGenerator;
 	}
 
 }
