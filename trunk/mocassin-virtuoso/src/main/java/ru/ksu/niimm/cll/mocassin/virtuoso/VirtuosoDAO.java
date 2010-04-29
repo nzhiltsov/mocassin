986037311eeee938673bcac4e41dcb12b6d88e5c
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2010-04-29 13:48:42 +0000

    added method to get triples

--- trunk/mocassin-virtuoso/src/main/java/ru/ksu/niimm/cll/mocassin/virtuoso/VirtuosoDAO.java
+++ trunk/mocassin-virtuoso/src/main/java/ru/ksu/niimm/cll/mocassin/virtuoso/VirtuosoDAO.java
@@ -2,6 +2,9 @@ package ru.ksu.niimm.cll.mocassin.virtuoso;
 
 import java.util.List;
 
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QuerySolution;
+
 /**
  * DAO for Virtuoso RDF store
  * 
@@ -9,9 +12,37 @@ import java.util.List;
  * 
  */
 public interface VirtuosoDAO {
+	/**
+	 * delete all the triples that relate to document with given URI
+	 * 
+	 * @param documentUri
+	 * @param graph
+	 */
 	void delete(String documentUri, RDFGraph graph);
 
+	/**
+	 * insert the triples into given graph
+	 * 
+	 * @param triples
+	 * @param graph
+	 */
 	void insert(List<RDFTriple> triples, RDFGraph graph);
 
+	/**
+	 * update the triples that relate to document with given URI
+	 * 
+	 * @param documentUri
+	 * @param triples
+	 * @param graph
+	 */
 	void update(String documentUri, List<RDFTriple> triples, RDFGraph graph);
+
+	/**
+	 * get resources by given query
+	 * 
+	 * @param query
+	 * @param graph
+	 * @return
+	 */
+	List<QuerySolution> get(Query query, RDFGraph graph);
 }
