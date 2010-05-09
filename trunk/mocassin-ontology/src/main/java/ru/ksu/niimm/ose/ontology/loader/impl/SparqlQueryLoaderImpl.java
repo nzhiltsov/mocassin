1fa4d2b0163418457c9b32a067f2b4f6a0c6e634
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2010-05-09 09:38:16 +0000

    fixed the wrong import

--- trunk/mocassin-ontology/src/main/java/ru/ksu/niimm/ose/ontology/loader/impl/SparqlQueryLoaderImpl.java
+++ trunk/mocassin-ontology/src/main/java/ru/ksu/niimm/ose/ontology/loader/impl/SparqlQueryLoaderImpl.java
@@ -1,22 +1,17 @@
 package ru.ksu.niimm.ose.ontology.loader.impl;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
-import com.ibm.icu.util.StringTokenizer;
-
 import ru.ksu.niimm.ose.ontology.loader.SparqlQueryLoader;
-import unittest.util.LoadPropertiesUtil;
+
+import com.ibm.icu.util.StringTokenizer;
 
 public class SparqlQueryLoaderImpl implements SparqlQueryLoader {
 	private static final String NAMES_PARAMETER = "scriptNames";
