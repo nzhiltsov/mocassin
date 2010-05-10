8682b909605b1118c3b6cc9c0b8c8e7cb2473e40
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2010-05-10 19:25:07 +0000

    refactored ontology loader; added ontology test [Issue 33]

--- mocassin-ontology/src/main/java/ru/ksu/niimm/ose/ontology/loader/impl/OMDocOntologyLoaderImpl.java
+++ mocassin-ontology/src/main/java/ru/ksu/niimm/ose/ontology/loader/impl/OMDocOntologyLoaderImpl.java
@@ -3,31 +3,28 @@ package ru.ksu.niimm.ose.ontology.loader.impl;
 import java.io.IOException;
 import java.io.InputStream;
 
+import ru.ksu.niimm.ose.ontology.loader.OMDocOntologyLoader;
+
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
-
-import ru.ksu.niimm.ose.ontology.loader.OMDocOntologyLoader;
 
 public class OMDocOntologyLoaderImpl implements OMDocOntologyLoader {
 	private static final String FILE_PATH = "/omdoc.owl";
 	private OntModel omdocOntology;
 
+	public OMDocOntologyLoaderImpl() throws IOException {
+		this.omdocOntology = load();
+	}
+
 	@Override
 	public OntModel getOntology() {
-		if (omdocOntology == null) {
-			try {
-				omdocOntology = load();
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-		}
 		return omdocOntology;
 	}
 
 	private OntModel load() throws IOException {
 		OntModel omdocOntology = ModelFactory
-				.createOntologyModel(OntModelSpec.OWL_DL_MEM_RULE_INF);
+				.createOntologyModel(OntModelSpec.OWL_DL_MEM_RDFS_INF);
 		InputStream inputStream = null;
 		try {
 			inputStream = this.getClass().getResourceAsStream(FILE_PATH);
@@ -36,7 +33,7 @@ public class OMDocOntologyLoaderImpl implements OMDocOntologyLoader {
 		} catch (Exception e) {
 			if (inputStream != null)
 				inputStream.close();
-			throw new IOException("cannot read ontology");
+			throw new IOException(String.format("cannot read ontology: %s", e));
 		}
 
 	}
