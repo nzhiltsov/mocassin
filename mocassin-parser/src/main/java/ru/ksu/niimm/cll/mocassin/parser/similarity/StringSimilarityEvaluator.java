266fe2ab97c94e8049b1110162899a70e14f9d17
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2010-07-13 15:34:31 +0000

    added string similarity evaluator

--- mocassin-parser/src/main/java/ru/ksu/niimm/cll/mocassin/parser/similarity/StringSimilarityEvaluator.java
+++ mocassin-parser/src/main/java/ru/ksu/niimm/cll/mocassin/parser/similarity/StringSimilarityEvaluator.java
@@ -2,7 +2,7 @@ package ru.ksu.niimm.cll.mocassin.parser.similarity;
 
 public interface StringSimilarityEvaluator {
 	enum SimilarityMetrics {
-		LEVENSHTEIN, SOUNDEX, N_GRAM
+		LEVENSHTEIN, SOUNDEX, N_GRAM, JARO_WINKLER, MONGE_ELKAN, NEEDLEMAN_WUNCH, SMITH_WATERMAN
 	}
 
 	float getSimilarity(String firstString, String secondString,
--- mocassin-parser/src/main/java/ru/ksu/niimm/cll/mocassin/parser/similarity/impl/StringSimilarityEvaluatorImpl.java
+++ mocassin-parser/src/main/java/ru/ksu/niimm/cll/mocassin/parser/similarity/impl/StringSimilarityEvaluatorImpl.java
@@ -5,8 +5,12 @@ import java.util.Map;
 
 import ru.ksu.niimm.cll.mocassin.parser.similarity.StringSimilarityEvaluator;
 import uk.ac.shef.wit.simmetrics.similaritymetrics.AbstractStringMetric;
+import uk.ac.shef.wit.simmetrics.similaritymetrics.JaroWinkler;
 import uk.ac.shef.wit.simmetrics.similaritymetrics.Levenshtein;
+import uk.ac.shef.wit.simmetrics.similaritymetrics.MongeElkan;
+import uk.ac.shef.wit.simmetrics.similaritymetrics.NeedlemanWunch;
 import uk.ac.shef.wit.simmetrics.similaritymetrics.QGramsDistance;
+import uk.ac.shef.wit.simmetrics.similaritymetrics.SmithWaterman;
 import uk.ac.shef.wit.simmetrics.similaritymetrics.Soundex;
 
 public class StringSimilarityEvaluatorImpl implements StringSimilarityEvaluator {
@@ -16,11 +20,21 @@ public class StringSimilarityEvaluatorImpl implements StringSimilarityEvaluator 
 		type2distance.put(SimilarityMetrics.LEVENSHTEIN, new Levenshtein());
 		type2distance.put(SimilarityMetrics.SOUNDEX, new Soundex());
 		type2distance.put(SimilarityMetrics.N_GRAM, new QGramsDistance());
+		type2distance.put(SimilarityMetrics.JARO_WINKLER, new JaroWinkler());
+		type2distance.put(SimilarityMetrics.MONGE_ELKAN, new MongeElkan());
+		type2distance.put(SimilarityMetrics.NEEDLEMAN_WUNCH,
+				new NeedlemanWunch());
+		type2distance
+				.put(SimilarityMetrics.SMITH_WATERMAN, new SmithWaterman());
 	}
 
 	@Override
 	public float getSimilarity(String firstString, String secondString,
 			SimilarityMetrics metricType) {
+		if (!getType2metric().containsKey(metricType)) {
+			throw new UnsupportedOperationException(String.format(
+					"this metric isn't supported: %s", metricType));
+		}
 		AbstractStringMetric distance = getType2metric().get(metricType);
 		return distance.getSimilarity(firstString, secondString);
 	}
