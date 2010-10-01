64e7490c12e56f9a662de775d204e8a1e0c1ffec
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2010-10-01 21:37:56 +0000

    refactored reading of LSI parameters [Issue 45]

---
+++ mocassin-analyzer/src/main/java/ru/ksu/niimm/cll/mocassin/analyzer/lsa/LSIPropertiesLoader.java
@@ -0,0 +1,26 @@
+package ru.ksu.niimm.cll.mocassin.analyzer.lsa;
+
+public interface LSIPropertiesLoader {
+	/**
+	 * should LSI filter out the stop words
+	 * 
+	 * @return
+	 */
+	boolean useStopWords();
+
+	int getMaxFactors();
+
+	double getFeatureInit();
+
+	double getInitialLearningRate();
+
+	int getAnilingRate();
+
+	double getRegularization();
+
+	double getMinImprovement();
+
+	int getMinEpochs();
+
+	int getMaxEpochs();
+}
