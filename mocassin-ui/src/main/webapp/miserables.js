51799e2fb5675ebed4405160c2e6a62777731b31
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2011-03-17 04:54:22 +0000

    added viewer UI

---
+++ mocassin-ui/src/main/webapp/miserables.js
@@ -0,0 +1,11 @@
+var miserables = {
+  nodes:[
+    {nodeName:"Theorem 1", group:1},
+    {nodeName:"Theorem 2", group:1},
+    {nodeName:"Proof", group:2}
+  ],
+  links:[
+    {source:2, target:0, value:1},
+    {source:1, target:0, value:8}
+  ]
+};
