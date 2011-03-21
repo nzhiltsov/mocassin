4defb5b4533d3e9d5a17417f5d0f96bf72bfa987
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2011-03-21 21:34:37 +0000

    started an implementation of storing arXiv metadata to RDF [Issue 53]

--- mocassin-ui/src/main/java/ru/ksu/niimm/cll/mocassin/ui/viewer/server/ArxivServiceImpl.java
+++ mocassin-ui/src/main/java/ru/ksu/niimm/cll/mocassin/ui/viewer/server/ArxivServiceImpl.java
@@ -1,7 +1,9 @@
 package ru.ksu.niimm.cll.mocassin.ui.viewer.server;
 
+import ru.ksu.niimm.cll.mocassin.arxiv.ArticleMetadata;
 import ru.ksu.niimm.cll.mocassin.arxiv.ArxivDAOFacade;
 import ru.ksu.niimm.cll.mocassin.ui.viewer.client.ArxivService;
+import ru.ksu.niimm.cll.mocassin.virtuoso.VirtuosoDAO;
 
 import com.google.inject.Inject;
 
@@ -9,9 +11,11 @@ public class ArxivServiceImpl implements ArxivService {
 
 	@Inject
 	private ArxivDAOFacade arxivDAOFacade;
+	@Inject
+	private VirtuosoDAO virtuosoDAO;
 
 	@Override
 	public void handle(String arxivId) {
-		arxivDAOFacade.retrieve(arxivId);
+		ArticleMetadata metadata = arxivDAOFacade.retrieve(arxivId);
 	}
 }
