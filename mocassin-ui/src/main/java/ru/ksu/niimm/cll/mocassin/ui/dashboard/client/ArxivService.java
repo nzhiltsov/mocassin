68964ea3873c0b2d80f4ed8996dd6cf03e67432f
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2011-03-24 20:49:55 +0000

    added upload and grid to Dashboard UI [Issue 54]

--- mocassin-ui/src/main/java/ru/ksu/niimm/cll/mocassin/ui/dashboard/client/ArxivService.java
+++ mocassin-ui/src/main/java/ru/ksu/niimm/cll/mocassin/ui/dashboard/client/ArxivService.java
@@ -1,4 +1,6 @@
-package ru.ksu.niimm.cll.mocassin.ui.viewer.client;
+package ru.ksu.niimm.cll.mocassin.ui.dashboard.client;
+
+import java.util.List;
 
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
@@ -6,4 +8,6 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 @RemoteServiceRelativePath("GWT.rpc")
 public interface ArxivService extends RemoteService {
 	public void handle(String arxivId);
+	
+	public List<ArxivArticleMetadata> loadArticles();
 }
--- mocassin-ui/src/main/java/ru/ksu/niimm/cll/mocassin/ui/dashboard/server/ArxivServiceImpl.java
+++ mocassin-ui/src/main/java/ru/ksu/niimm/cll/mocassin/ui/dashboard/server/ArxivServiceImpl.java
@@ -1,13 +1,16 @@
-package ru.ksu.niimm.cll.mocassin.ui.viewer.server;
+package ru.ksu.niimm.cll.mocassin.ui.dashboard.server;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ru.ksu.niimm.cll.mocassin.arxiv.ArticleMetadata;
 import ru.ksu.niimm.cll.mocassin.arxiv.ArxivDAOFacade;
-import ru.ksu.niimm.cll.mocassin.ui.viewer.client.ArxivService;
+import ru.ksu.niimm.cll.mocassin.ui.dashboard.client.ArxivArticleMetadata;
+import ru.ksu.niimm.cll.mocassin.ui.dashboard.client.ArxivService;
 import ru.ksu.niimm.cll.mocassin.ui.viewer.server.util.ArxivMetadataUtil;
 import ru.ksu.niimm.cll.mocassin.virtuoso.RDFTriple;
 import ru.ksu.niimm.cll.mocassin.virtuoso.VirtuosoDAO;
+
 
 import com.google.inject.Inject;
 
@@ -24,4 +27,13 @@ public class ArxivServiceImpl implements ArxivService {
 		List<RDFTriple> triples = ArxivMetadataUtil.convertToTriples(metadata);
 		virtuosoDAO.insert(triples);
 	}
+
+	@Override
+	public List<ArxivArticleMetadata> loadArticles() {
+		List<ArxivArticleMetadata> list = new ArrayList<ArxivArticleMetadata>();
+				list.add(new ArxivArticleMetadata("math/0205003v1",
+						"A strengthening of the Nyman-Beurling criterion for the Riemann hypothesis, 2"));
+		return list;
+	}
+
 }
