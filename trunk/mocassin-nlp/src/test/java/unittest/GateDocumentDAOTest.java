69e59e457ef01b15a68159f718aae3ae85970489
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2011-04-06 15:41:39 +0000

    fixed the test

--- trunk/mocassin-nlp/src/test/java/unittest/GateDocumentDAOTest.java
+++ trunk/mocassin-nlp/src/test/java/unittest/GateDocumentDAOTest.java
@@ -11,14 +11,18 @@ import org.junit.runner.RunWith;
 
 import ru.ksu.niimm.cll.mocassin.nlp.NlpModule;
 import ru.ksu.niimm.cll.mocassin.nlp.gate.GateDocumentDAO;
+import ru.ksu.niimm.cll.mocassin.parser.LatexParserModule;
 import ru.ksu.niimm.cll.mocassin.util.CollectionUtil;
+import ru.ksu.niimm.cll.mocassin.virtuoso.VirtuosoModule;
+import ru.ksu.niimm.ose.ontology.OntologyModule;
 
 import com.google.inject.Inject;
 import com.mycila.testing.junit.MycilaJunitRunner;
 import com.mycila.testing.plugin.guice.GuiceContext;
 
 @RunWith(MycilaJunitRunner.class)
-@GuiceContext(NlpModule.class)
+@GuiceContext( { NlpModule.class, OntologyModule.class, VirtuosoModule.class,
+		LatexParserModule.class })
 public class GateDocumentDAOTest {
 	@Inject
 	private Logger logger;
@@ -27,7 +31,8 @@ public class GateDocumentDAOTest {
 
 	@Test
 	public void testLoad() throws Exception {
-		List<String> ids = CollectionUtil.sampleRandomSublist(getGateDocumentDAO().getDocumentIds(), 10);
+		List<String> ids = CollectionUtil.sampleRandomSublist(
+				getGateDocumentDAO().getDocumentIds(), 10);
 		for (String id : ids) {
 			Document document = null;
 			try {
