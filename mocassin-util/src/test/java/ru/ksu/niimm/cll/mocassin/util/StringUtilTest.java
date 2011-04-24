427600507125cec7446c9bdeefadda47b558555a
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2011-04-24 23:53:48 +0000

    fixed cleaning the text contents; organized imports[Issue 59]

--- mocassin-util/src/test/java/ru/ksu/niimm/cll/mocassin/util/StringUtilTest.java
+++ mocassin-util/src/test/java/ru/ksu/niimm/cll/mocassin/util/StringUtilTest.java
@@ -9,11 +9,11 @@ import org.junit.Test;
 public class StringUtilTest {
 	@Test
 	public void testStripLatexMarkup() {
-		String str = "\\cite{this} \\ref{that} the fan of $Y$ coincides with the \textit{restriction} of the fan of $X$ to the subspace $\\Pic{(Y)}_{\\br}\\subset\\Pic{(X)}_{\\br}$: i.e.";
+		String str = ".\\label{nef=sa} $\\Pic{(X)}_{\\bq} \\simeq \\Num{(X)}_{\\bq}$ $\bq$-factorial preserving-order \\cite[Proposition 1.11(3)]{this} \\ref{that} (\ref{fin_pic}) the fan of $Y$ coincides with the \textit{restriction} of the fan of $X$ to the subspace $\\Pic{(Y)}_{\\br}\\subset\\Pic{(X)}_{\\br}$: i.e. proof.";
 		List<String> strippedTokens = StringUtil.stripLatexMarkup(str);
-		String[] expected = { "the", "fan", "of", "coincides", "with", "the",
+		String[] expected = {"factorial", "preserving", "order", "the", "fan", "of", "coincides", "with", "the",
 				"restriction", "of", "the", "fan", "of", "to", "the",
-				"subspace" };
+				"subspace", "ie", "proof" };
 		Assert.assertEquals(strippedTokens.size(), expected.length);
 		for (int i = 0; i < strippedTokens.size(); i++) {
 			Assert.assertEquals(strippedTokens.get(i), expected[i]);
