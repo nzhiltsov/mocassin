a23f0843b74023a366ac6dde8a6529662db095cc
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2011-08-05 08:02:36 +0000

    refactored

--- trunk/mocassin-analyzer/src/test/java/unittest/StructuralElementTypeAnalyzerTest.java
+++ trunk/mocassin-analyzer/src/test/java/unittest/StructuralElementTypeAnalyzerTest.java
@@ -7,7 +7,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import ru.ksu.niimm.cll.mocassin.nlp.Reference;
-import ru.ksu.niimm.cll.mocassin.nlp.recognizer.StructuralElementTypeRecognizer;
+import ru.ksu.niimm.cll.mocassin.nlp.StructuralElementTypeRecognizer;
 import ru.ksu.niimm.cll.mocassin.ontology.MocassinOntologyClasses;
 import unittest.info.PredictedPairInfo;
 
