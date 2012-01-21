1216c827fb776d9c1616803232f0540f1969d296
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2012-01-21 18:39:40 +0000

    added the test for the 'load' method in Latex DAO

--- trunk/mocassin-parser/src/test/java/ru/ksu/niimm/cll/mocassin/parser/latex/LatexDocumentDAOTest.java
+++ trunk/mocassin-parser/src/test/java/ru/ksu/niimm/cll/mocassin/parser/latex/LatexDocumentDAOTest.java
@@ -10,6 +10,8 @@ import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import junit.framework.Assert;
+
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -27,52 +29,12 @@ import com.mycila.testing.plugin.guice.GuiceContext;
 @GuiceContext({ LatexParserModule.class, PdfParserModule.class })
 public class LatexDocumentDAOTest {
 	@Inject
-	Logger logger;
-	@Inject
 	private LatexDocumentDAO latexDocumentDAO;
-	/**
-	 * e.g. <"ivm227", "1997/04/01-4.TEX">
-	 */
-	private final Map<String, String> id2filename = Maps.newHashMap();
 
-	@Before
-	public void init() throws IOException {
-		CsvReader reader = new CsvReader(
-				new InputStreamReader(this.getClass().getClassLoader()
-						.getResourceAsStream("mathnet_izvestiya.csv")), ';');
-		reader.setTrimWhitespace(true);
-		try {
-			while (reader.readRecord()) {
-				String id = reader.get(0);
-				String filename = reader.get(1);
-				StringTokenizer st = new StringTokenizer(filename, "-.");
-				String name = st.nextToken();
-				String volume = st.nextToken();
-				String year = st.nextToken();
-				String extension = st.nextToken();
-				String filepath = String.format("%s/%s/%s-%s.%s", year,
-						volume.length() == 1 ? "0" + volume : volume, name,
-						volume, extension);
-				id2filename.put(id, filepath);
-			}
-		} finally {
-			reader.close();
-		}
+	@Test
+	public void testLoad() {
+
+		LatexDocumentModel doc = latexDocumentDAO.load("ivm18");
+		Assert.assertEquals(68, doc.getReferences().size());
 	}
-
-	@Test @Ignore
-	public void testSaveMathnetArticles() {
-		for (Map.Entry<String, String> entry : id2filename.entrySet()) {
-			try {
-				latexDocumentDAO.save(
-						entry.getKey(),
-						new FileInputStream(new File(
-								"/opt/mocassin/IzvestiyaVuzov-tex/"
-										+ entry.getValue())), "cp866");
-			} catch (FileNotFoundException e) {
-				logger.log(Level.SEVERE, e.getMessage());
-			}
-		}
-	}
-
 }
