95a2a45804203cca03ca9b200c24172484e70290
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2012-01-23 10:54:19 +0000

    fixed the test

--- trunk/mocassin-analyzer/src/test/java/unittest/NameMatcherTest.java
+++ trunk/mocassin-analyzer/src/test/java/unittest/NameMatcherTest.java
@@ -14,12 +14,14 @@ import ru.ksu.niimm.cll.mocassin.analyzer.mapping.MappingElement;
 import ru.ksu.niimm.cll.mocassin.analyzer.mapping.matchers.Matcher;
 import ru.ksu.niimm.cll.mocassin.fulltext.FullTextModule;
 import ru.ksu.niimm.cll.mocassin.nlp.NlpModule;
+import ru.ksu.niimm.cll.mocassin.nlp.gate.GateModule;
 import ru.ksu.niimm.cll.mocassin.parser.latex.Edge;
 import ru.ksu.niimm.cll.mocassin.parser.latex.LatexDocumentModel;
 import ru.ksu.niimm.cll.mocassin.parser.latex.LatexParserModule;
 import ru.ksu.niimm.cll.mocassin.parser.latex.Node;
 import ru.ksu.niimm.cll.mocassin.parser.latex.Parser;
 import ru.ksu.niimm.cll.mocassin.parser.latex.StructureBuilder;
+import ru.ksu.niimm.cll.mocassin.parser.pdf.PdfParserModule;
 import ru.ksu.niimm.cll.mocassin.virtuoso.VirtuosoModule;
 import ru.ksu.niimm.ose.ontology.OntologyModule;
 
@@ -32,7 +34,8 @@ import edu.uci.ics.jung.graph.Graph;
 
 @RunWith(MycilaJunitRunner.class)
 @GuiceContext({ AnalyzerModule.class, NlpModule.class, LatexParserModule.class,
-		OntologyModule.class, VirtuosoModule.class, FullTextModule.class })
+		OntologyModule.class, VirtuosoModule.class, FullTextModule.class,
+		GateModule.class, PdfParserModule.class })
 public class NameMatcherTest {
 	@Inject
 	private Parser parser;
