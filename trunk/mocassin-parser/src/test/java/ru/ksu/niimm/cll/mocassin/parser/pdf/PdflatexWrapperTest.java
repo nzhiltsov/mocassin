cdf2aa4815e708501ee0a0d82ba2a44173a68efa
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2012-02-09 20:17:24 +0000

    added checking if the generated PDF of the expected number of pages in the test

--- trunk/mocassin-parser/src/test/java/ru/ksu/niimm/cll/mocassin/parser/pdf/PdflatexWrapperTest.java
+++ trunk/mocassin-parser/src/test/java/ru/ksu/niimm/cll/mocassin/parser/pdf/PdflatexWrapperTest.java
@@ -1,28 +1,22 @@
 package ru.ksu.niimm.cll.mocassin.parser.pdf;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.LineNumberReader;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.StringTokenizer;
 import java.util.logging.Logger;
 
 import junit.framework.Assert;
 
-import org.junit.Before;
-import org.junit.Ignore;
+import org.apache.pdfbox.pdmodel.PDDocument;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import ru.ksu.niimm.cll.mocassin.parser.latex.LatexParserModule;
 import ru.ksu.niimm.cll.mocassin.util.StringUtil;
 
-import com.csvreader.CsvReader;
-import com.google.common.collect.Maps;
 import com.google.inject.Inject;
 import com.mycila.testing.junit.MycilaJunitRunner;
 import com.mycila.testing.plugin.guice.GuiceContext;
@@ -37,47 +31,17 @@ public class PdflatexWrapperTest {
 	@Inject
 	private PdflatexWrapper pdflatexWrapper;
 
-	/**
-	 * e.g. <"ivm227", "1997/04/01-4.TEX">
-	 */
-	private final Map<String, String> id2filename = Maps.newHashMap();
-
-	@Before
-	public void init() throws IOException {
-		CsvReader reader = new CsvReader(
-				new InputStreamReader(this.getClass().getClassLoader()
-						.getResourceAsStream("mathnet_izvestiya.csv")), ';');
-		reader.setTrimWhitespace(true);
-		try {
-			while (reader.readRecord()) {
-				String id = reader.get(0);
-				String filename = reader.get(1);
-				StringTokenizer st = new StringTokenizer(filename, "-.");
-				String name = st.nextToken();
-				String volume = st.nextToken();
-				String year = st.nextToken();
-				String extension = st.nextToken();
-				String filepath = String.format("%s/%s/%s-%s.%s", year,
-						volume.length() == 1 ? "0" + volume : volume, name,
-						volume, extension);
-				id2filename.put(id, filepath);
-			}
-		} finally {
-			reader.close();
-		}
-	}
-
 	@Test
 	public void testCompilePatched() throws PdflatexCompilationException,
 			IOException {
 		String collectionId = "ivm18";
 		pdflatexWrapper.compilePatched(collectionId);
-		checkIfPDFGenerated(collectionId);
+		checkIfPDFGenerated(collectionId, 7);
 		checkIfPdfsyncGenerated(collectionId, SEVENTH_PAGE_MARK);
 
 		collectionId = "ivm537";
 		pdflatexWrapper.compilePatched(collectionId);
-		checkIfPDFGenerated(collectionId);
+		checkIfPDFGenerated(collectionId, 5);
 		checkIfPdfsyncGenerated(collectionId, FIFTH_PAGE_MARK);
 	}
 
@@ -91,12 +55,22 @@ public class PdflatexWrapperTest {
 		checkIfFound(pdfsyncFile, mark);
 	}
 
-	private static void checkIfPDFGenerated(String collectionId) {
+	private static void checkIfPDFGenerated(String collectionId,
+			int expectedNumberOfPages) throws FileNotFoundException,
+			IOException {
 		File pdfFile = new File("/opt/mocassin/aux-pdf/"
 				+ StringUtil.arxivid2filename(collectionId, "pdf"));
 		Assert.assertTrue(String.format(
 				"The PDF file='%s' hasn't been generated.", pdfFile.getName()),
 				pdfFile.exists());
+		PDDocument pdfDoc = PDDocument.load(new FileInputStream(pdfFile));
+		try {
+			Assert.assertEquals(
+					"Number of pages in the generated document does not equal to the expected one",
+					expectedNumberOfPages, pdfDoc.getNumberOfPages());
+		} finally {
+			pdfDoc.close();
+		}
 	}
 
 	private static void checkIfFound(File file, String mark) throws IOException {
