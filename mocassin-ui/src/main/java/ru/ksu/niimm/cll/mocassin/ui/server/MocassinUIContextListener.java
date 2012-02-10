d439c907382ded007648cd598c14cae045be25ea
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2012-02-10 19:00:26 +0000

    switched to using virtuoso triple store on production

--- mocassin-ui/src/main/java/ru/ksu/niimm/cll/mocassin/ui/server/MocassinUIContextListener.java
+++ mocassin-ui/src/main/java/ru/ksu/niimm/cll/mocassin/ui/server/MocassinUIContextListener.java
@@ -2,11 +2,9 @@ package ru.ksu.niimm.cll.mocassin.ui.server;
 
 import ru.ksu.niimm.cll.mocassin.analyzer.AnalyzerModule;
 import ru.ksu.niimm.cll.mocassin.arxiv.ArxivModule;
-import ru.ksu.niimm.cll.mocassin.fulltext.FullTextModule;
 import ru.ksu.niimm.cll.mocassin.nlp.NlpModule;
 import ru.ksu.niimm.cll.mocassin.nlp.gate.GateModule;
 import ru.ksu.niimm.cll.mocassin.ontology.OntologyModule;
-import ru.ksu.niimm.cll.mocassin.ontology.OntologyTestModule;
 import ru.ksu.niimm.cll.mocassin.parser.latex.LatexParserModule;
 import ru.ksu.niimm.cll.mocassin.parser.pdf.PdfParserModule;
 import ru.ksu.niimm.cll.mocassin.virtuoso.VirtuosoModule;
@@ -20,7 +18,7 @@ public class MocassinUIContextListener extends GuiceServletContextListener {
 	@Override
 	protected Injector getInjector() {
 		return Guice.createInjector(new MocassinUIModule(),
-				new OntologyTestModule(), new VirtuosoModule(),
+				new OntologyModule(), new VirtuosoModule(),
 				new ArxivModule(), new NlpModule(), new GateModule(),
 				new LatexParserModule(), new AnalyzerModule(),
 				new PdfParserModule());
