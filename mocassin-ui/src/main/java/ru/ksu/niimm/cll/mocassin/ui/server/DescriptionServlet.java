83b52ae94b154f198bda60b94405203cfc5778f7
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2012-02-15 20:50:59 +0000

    set up logging for 'UI' module [Issue 80]

--- mocassin-ui/src/main/java/ru/ksu/niimm/cll/mocassin/ui/server/DescriptionServlet.java
+++ mocassin-ui/src/main/java/ru/ksu/niimm/cll/mocassin/ui/server/DescriptionServlet.java
@@ -1,7 +1,6 @@
 package ru.ksu.niimm.cll.mocassin.ui.server;
 
 import java.io.IOException;
-import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -9,7 +8,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+
 import ru.ksu.niimm.cll.mocassin.ontology.QueryManagerFacade;
+import ru.ksu.niimm.cll.mocassin.util.inject.log.InjectLogger;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -19,7 +21,7 @@ import com.google.inject.Singleton;
 public class DescriptionServlet extends HttpServlet {
 	private static final String RESOURCE_URI_PARAMETER_NAME = "resourceuri";
 	private static final String CONTENT_TYPE = "application/rdf+xml; charset=UTF-8";
-	@Inject
+	@InjectLogger
 	private Logger logger;
 	@Inject
 	private QueryManagerFacade queryManagerFacade;
@@ -33,6 +35,9 @@ public class DescriptionServlet extends HttpServlet {
 		String model = getQueryManagerFacade().describe(resourceUri);
 		outputStream.print(model);
 		outputStream.close();
+		logger.debug(
+				"Request for a resource='{}' has been processed successfully",
+				resourceUri);
 	}
 
 	private QueryManagerFacade getQueryManagerFacade() {
