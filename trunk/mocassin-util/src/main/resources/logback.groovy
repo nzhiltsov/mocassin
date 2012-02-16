96847aa76a76b186341e77024581c29bc2224892
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2012-02-16 09:57:32 +0000

    configure logging [Issue 80]

--- trunk/mocassin-util/src/main/resources/logback.groovy
+++ trunk/mocassin-util/src/main/resources/logback.groovy
@@ -1,16 +1,28 @@
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.core.ConsoleAppender
 import ch.qos.logback.core.FileAppender
+import ch.qos.logback.core.status.OnConsoleStatusListener
 
 import static ch.qos.logback.classic.Level.DEBUG
 import static ch.qos.logback.classic.Level.INFO
 
+statusListener(OnConsoleStatusListener)
+
+def USER_HOME = System.getProperty("user.home")
 def MOCASSIN_HOME = "/opt/mocassin"
 
+def isProduction = false
+if (USER_HOME =~ /tomcat/) {
+	isProduction = true
+	println "Production logging mode is initialized"
+}
+
+if (!isProduction) {
 appender("STDOUT", ConsoleAppender) {
   encoder(PatternLayoutEncoder) {
     pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
   }
+}
 }
 appender("FILE", FileAppender) {
 	file = "${MOCASSIN_HOME}/logs/common.log"
@@ -18,7 +30,13 @@ appender("FILE", FileAppender) {
 	  pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
 	}
   }
+
 logger("com.hp.hpl.jena.util", INFO)
 logger("o.o.query.parser", INFO)
 logger("org.openrdf.rio", INFO)
+
+if (isProduction) {
+	root(INFO, ["FILE"])
+} else {
 root(INFO, ["FILE", "STDOUT"])
+}
