7a2150e720b51543ec139e3e3f6834e60d661be6
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2012-02-17 10:14:01 +0000

    refactored to three available logging modes

--- trunk/mocassin-util/src/main/resources/logback.groovy
+++ trunk/mocassin-util/src/main/resources/logback.groovy
@@ -8,36 +8,60 @@ import static ch.qos.logback.classic.Level.INFO
 
 statusListener(OnConsoleStatusListener)
 
+def HOSTNAME=hostname
 def USER_HOME = System.getProperty("user.home")
 def MOCASSIN_HOME = "/opt/mocassin"
-def productionFlag = "linglab"
+def productionFlag = "server318"
+def testFlag = "tomcat"
 
-def isProduction = false
-if (USER_HOME.contains(productionFlag)) {
-	isProduction = true
-	println "Production logging mode is initialized"
+def PATTERN = "%d{HH:mm:ss.SSS}\t[%thread]\t%-5level\t%logger{36}\t-\t%msg%n"
+
+enum Mode {
+	DEV, TEST, PRODUCTION
 }
 
-if (!isProduction) {
-appender("STDOUT", ConsoleAppender) {
-  encoder(PatternLayoutEncoder) {
-    pattern = "%d{HH:mm:ss.SSS}\t[%thread]\t%-5level\t%logger{36}\t-\t%msg%n"
-  }
+def mode = Mode.DEV
+
+if (HOSTNAME.contains(productionFlag) & USER_HOME.contains(testFlag)) {
+	mode = Mode.TEST
+	println "Test logging mode will be initialized"
+} else if (HOSTNAME.contains(productionFlag)) {
+	mode = Mode.PRODUCTION
+	println "Production logging mode will be initialized"
+} else {
+println "Development logging mode will be initialized"
 }
+
+switch (mode) {
+	case Mode.DEV:
+		appender("STDOUT", ConsoleAppender) {
+			encoder(PatternLayoutEncoder) { pattern = PATTERN }
+		}
+		root(DEBUG, ["STDOUT"])
+		break
+
+	case Mode.TEST:
+		appender("TESTFILE", FileAppender) {
+			file = "${MOCASSIN_HOME}/logs/common-test.log"
+			append = false
+			encoder(PatternLayoutEncoder) { pattern = PATTERN }
+		}
+		root(DEBUG, ["TESTFILE"])
+		break
+
+	case Mode.PRODUCTION:
+		appender("FILE", FileAppender) {
+			file = "${MOCASSIN_HOME}/logs/common.log"
+			encoder(PatternLayoutEncoder) { pattern = PATTERN }
+		}
+		root(INFO, ["FILE"])
+		break
+
+	default:
+		println "Failed to recognize the logging mode. See logback.groovy file."
+		break
 }
-appender("FILE", FileAppender) {
-	file = "${MOCASSIN_HOME}/logs/common.log"
-	encoder(PatternLayoutEncoder) {
-	  pattern = "%d{HH:mm:ss.SSS}\t[%thread]\t%-5level\t%logger{36}\t-\t%msg%n"
-	}
-  }
 
 logger("com.hp.hpl.jena.util", INFO)
 logger("o.o.query.parser", INFO)
 logger("org.openrdf.rio", INFO)
-
-if (isProduction) {
-	root(INFO, ["FILE"])
-} else {
-root(DEBUG, ["STDOUT"])
-}
