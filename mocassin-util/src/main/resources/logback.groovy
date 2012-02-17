a2ff20f3118ce9252febf6225ea63596dbe22c8a
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2012-02-17 11:40:47 +0000

    fixed import in the logging configuration script

--- mocassin-util/src/main/resources/logback.groovy
+++ mocassin-util/src/main/resources/logback.groovy
@@ -2,6 +2,7 @@ import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.core.ConsoleAppender
 import ch.qos.logback.core.rolling.RollingFileAppender
 import ch.qos.logback.core.status.OnConsoleStatusListener
+import ch.qos.logback.core.rolling.TimeBasedRollingPolicy
 
 import static ch.qos.logback.classic.Level.DEBUG
 import static ch.qos.logback.classic.Level.INFO
