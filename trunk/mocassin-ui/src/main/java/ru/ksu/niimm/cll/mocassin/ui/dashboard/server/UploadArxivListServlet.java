af8ba65a23bcc398443b73125b7506ce89642cc7
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2012-02-20 10:47:38 +0000

    added execution of batch indexing in the different thread

--- trunk/mocassin-ui/src/main/java/ru/ksu/niimm/cll/mocassin/ui/dashboard/server/UploadArxivListServlet.java
+++ trunk/mocassin-ui/src/main/java/ru/ksu/niimm/cll/mocassin/ui/dashboard/server/UploadArxivListServlet.java
@@ -2,6 +2,8 @@ package ru.ksu.niimm.cll.mocassin.ui.dashboard.server;
 
 import java.io.IOException;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,6 +19,11 @@ import org.slf4j.Logger;
 import ru.ksu.niimm.cll.mocassin.util.IOUtil;
 import ru.ksu.niimm.cll.mocassin.util.inject.log.InjectLogger;
 
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
@@ -37,20 +44,39 @@ public class UploadArxivListServlet extends HttpServlet {
 			FileItemIterator it = upload.getItemIterator(req);
 			if (it.hasNext()) {
 				FileItemStream item = it.next();
-				Set<String> ids = IOUtil.readLineSet(item.openStream());
+				final Set<String> ids = IOUtil.readLineSet(item.openStream());
 				logger.debug("Found {} identifiers to upload in the request",
 						ids.size());
-				int numberOfSuccesses = arXMLivAdapter.handle(ids);
-				logger.debug(
-						"{} out of {} document(s) have been processed successfully",
-						numberOfSuccesses, ids.size());
+				ListeningExecutorService service = MoreExecutors
+						.listeningDecorator(Executors.newSingleThreadExecutor());
+				ListenableFuture<Integer> indexing = service
+						.submit(new Callable<Integer>() {
+							public Integer call() {
+								return arXMLivAdapter.handle(ids);
+							}
+						});
+				Futures.addCallback(indexing, new FutureCallback<Integer>() {
+					public void onSuccess(Integer numberOfSuccesses) {
+						logger.debug(
+								"{} document(s) have been processed successfully",
+								numberOfSuccesses);
+					}
+
+					public void onFailure(Throwable thrown) {
+						logger.error("Failed to process uploaded documents",
+								thrown);
+					}
+				});
 				resp.setContentType("text/html");
-				resp.getWriter().printf("{ \"numberOfSuccesses\": %d}",
-						numberOfSuccesses);
+				resp.getWriter()
+						.printf("The documents from the uploaded list have been scheduled for processing.");
 			}
 		} catch (FileUploadException e) {
 			logger.error(
 					"Failed to upload a list of articles ids for processing", e);
+			resp.setContentType("text/html");
+			resp.getWriter().printf(
+					"Failed to schedule the documents from the uploaded list.");
 		}
 
 	}
