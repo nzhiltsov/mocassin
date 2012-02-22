a194d659e07a5a452eb254a46fefc4b311c7a361
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2012-02-22 13:25:43 +0000

    fixed checking if the closest predecessor is not null

--- mocassin-analyzer/src/main/java/ru/ksu/niimm/cll/mocassin/analyzer/relation/impl/HasConsequenceRelationAnalyzerImpl.java
+++ mocassin-analyzer/src/main/java/ru/ksu/niimm/cll/mocassin/analyzer/relation/impl/HasConsequenceRelationAnalyzerImpl.java
@@ -1,5 +1,7 @@
 package ru.ksu.niimm.cll.mocassin.analyzer.relation.impl;
 
+import org.slf4j.Logger;
+
 import ru.ksu.niimm.cll.mocassin.analyzer.relation.HasConsequenceRelationAnalyzer;
 import ru.ksu.niimm.cll.mocassin.nlp.ParsedDocument;
 import ru.ksu.niimm.cll.mocassin.nlp.Reference;
@@ -9,6 +11,7 @@ import ru.ksu.niimm.cll.mocassin.nlp.impl.ReferenceImpl;
 import ru.ksu.niimm.cll.mocassin.nlp.impl.StructuralElementImpl.TypePredicate;
 import ru.ksu.niimm.cll.mocassin.ontology.MocassinOntologyClasses;
 import ru.ksu.niimm.cll.mocassin.ontology.MocassinOntologyRelations;
+import ru.ksu.niimm.cll.mocassin.util.inject.log.InjectLogger;
 
 import com.google.common.collect.Iterables;
 import com.google.inject.Inject;
@@ -17,6 +20,9 @@ import edu.uci.ics.jung.graph.Graph;
 
 public class HasConsequenceRelationAnalyzerImpl extends
 		AbstractRelationAnalyzer implements HasConsequenceRelationAnalyzer {
+	@InjectLogger
+	private Logger logger;
+
 	@Inject
 	protected HasConsequenceRelationAnalyzerImpl(
 			StructuralElementSearcher structuralElementSearcher) {
@@ -24,8 +30,8 @@ public class HasConsequenceRelationAnalyzerImpl extends
 	}
 
 	@Override
-	public void addRelations(
-			Graph<StructuralElement, Reference> graph, ParsedDocument document) {
+	public void addRelations(Graph<StructuralElement, Reference> graph,
+			ParsedDocument document) {
 		this.graph = graph;
 
 		Iterable<StructuralElement> corollaries = Iterables.filter(graph
@@ -39,11 +45,17 @@ public class HasConsequenceRelationAnalyzerImpl extends
 		for (StructuralElement corollary : corollaries) {
 			StructuralElement closestPredecessor = this.structuralElementSearcher
 					.findClosestPredecessor(corollary, validDomains, graph);
-			Reference reference = new ReferenceImpl.Builder(refId--).document(
-					document).build();
-			reference
-					.setPredictedRelation(MocassinOntologyRelations.HAS_CONSEQUENCE);
-			addEdge(reference, closestPredecessor, corollary);
+			if (closestPredecessor != null) {
+				Reference reference = new ReferenceImpl.Builder(refId--)
+						.document(document).build();
+				reference
+						.setPredictedRelation(MocassinOntologyRelations.HAS_CONSEQUENCE);
+				addEdge(reference, closestPredecessor, corollary);
+			} else {
+				logger.warn(
+						"Failed to add a relation for a corollary={}, because the found closest predecessor is null",
+						corollary.getUri());
+			}
 		}
 
 	}
