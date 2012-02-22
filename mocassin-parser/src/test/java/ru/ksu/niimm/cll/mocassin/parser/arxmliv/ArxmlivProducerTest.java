f696398c2eaa9bc8ae31cacc1781139ed7bc7663
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2012-02-22 15:16:09 +0000

    fixed the test

--- mocassin-parser/src/test/java/ru/ksu/niimm/cll/mocassin/parser/arxmliv/ArxmlivProducerTest.java
+++ mocassin-parser/src/test/java/ru/ksu/niimm/cll/mocassin/parser/arxmliv/ArxmlivProducerTest.java
@@ -1,15 +1,9 @@
 package ru.ksu.niimm.cll.mocassin.parser.arxmliv;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
 
 import junit.framework.Assert;
 
-import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,7 +11,6 @@ import ru.ksu.niimm.cll.mocassin.parser.latex.LatexParserModule;
 import ru.ksu.niimm.cll.mocassin.parser.pdf.PdfParserModule;
 import ru.ksu.niimm.cll.mocassin.util.StringUtil;
 
-import com.csvreader.CsvReader;
 import com.google.inject.Inject;
 import com.mycila.testing.junit.MycilaJunitRunner;
 import com.mycila.testing.plugin.guice.GuiceContext;
@@ -27,25 +20,6 @@ import com.mycila.testing.plugin.guice.GuiceContext;
 public class ArxmlivProducerTest {
 	@Inject
 	private ArxmlivProducer arxmlivProducer;
-
-	
-	private final List<String> ids = new LinkedList<String>();
-
-	@Before
-	public void init() throws IOException {
-		CsvReader reader = new CsvReader(
-				new InputStreamReader(this.getClass().getClassLoader()
-						.getResourceAsStream("mathnet_selected.csv")));
-		reader.setTrimWhitespace(true);
-		try {
-			while (reader.readRecord()) {
-				String id = reader.get(0);
-				ids.add(id);
-			}
-		} finally {
-			reader.close();
-		}
-	}
 
 	@Test
 	public void testProduce() {
@@ -59,15 +33,5 @@ public class ArxmlivProducerTest {
 		Assert.assertEquals("/opt/mocassin/arxmliv/ivm537.tex.xml", path);
 		Assert.assertTrue(new File("/opt/mocassin/arxmliv/"
 				+ StringUtil.arxivid2filename(arxivId, "tex.xml")).exists());
-	}
-
-	@Test @Ignore
-	public void testProduceForMathnetArticles() {
-		for (String mathnetKey : ids) {
-			try {
-				arxmlivProducer.produce(mathnetKey);
-			} catch (Exception e) { // nothing
-			}
-		}
 	}
 }
