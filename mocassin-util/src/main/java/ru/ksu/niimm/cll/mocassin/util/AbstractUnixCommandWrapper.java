06f1bfefca2e1cc5fb02572dd864f7ed17b51e39
nikita.zhiltsov <nikita.zhiltsov@gmail.com>
2012-03-06 11:48:00 +0000

    tagged the module

---
+++ mocassin-util/src/main/java/ru/ksu/niimm/cll/mocassin/util/AbstractUnixCommandWrapper.java
@@ -0,0 +1,40 @@
+package ru.ksu.niimm.cll.mocassin.util;
+
+import expectj.ExpectJ;
+import expectj.Spawn;
+
+public abstract class AbstractUnixCommandWrapper {
+	private static final long TIMEOUT_IN_SECONDS = 180;
+	protected final String[] cmdArray;
+	private final String successFlag;
+
+	public AbstractUnixCommandWrapper(int arraySize, String successFlag) {
+		this.cmdArray = new String[arraySize];
+		this.successFlag = successFlag;
+	}
+
+	public AbstractUnixCommandWrapper(int arraySize) {
+		this.cmdArray = new String[arraySize];
+		this.successFlag = null;
+	}
+
+	/**
+	 * 
+	 * @return true, if execution was successful
+	 * @throws Exception
+	 */
+	public final boolean execute() throws Exception {
+		ExpectJ expectinator = new ExpectJ(TIMEOUT_IN_SECONDS);
+
+		Spawn shell = expectinator.spawn(StringUtil.asString(cmdArray));
+		shell.expectClose();
+		String errOutput = shell.getCurrentStandardErrContents();
+		String standardOutput = shell.getCurrentStandardOutContents();
+		if (successFlag != null) {
+			boolean contains = standardOutput.contains(successFlag)
+					|| errOutput.contains(successFlag);
+			return contains;
+		}
+		return true;
+	}
+}
