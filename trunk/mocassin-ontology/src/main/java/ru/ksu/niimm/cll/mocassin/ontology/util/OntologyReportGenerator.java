c3d9e9201df8c9d24a8aa3e9a6c38d155d703f98
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2012-04-01 15:24:56 +0000

    implemented numbering for the reporter

--- trunk/mocassin-ontology/src/main/java/ru/ksu/niimm/cll/mocassin/ontology/util/OntologyReportGenerator.java
+++ trunk/mocassin-ontology/src/main/java/ru/ksu/niimm/cll/mocassin/ontology/util/OntologyReportGenerator.java
@@ -2,66 +2,127 @@ package ru.ksu.niimm.cll.mocassin.ontology.util;
 
 import static java.lang.String.format;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.Stack;
 import java.util.regex.Pattern;
 
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
 
+import com.google.common.collect.Maps;
+import com.hp.hpl.jena.ontology.AllValuesFromRestriction;
+import com.hp.hpl.jena.ontology.ObjectProperty;
 import com.hp.hpl.jena.ontology.OntClass;
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntProperty;
 import com.hp.hpl.jena.util.iterator.ExtendedIterator;
 
 public final class OntologyReportGenerator {
+	private static final String OWL_THING_URI = "http://www.w3.org/2002/07/owl#Thing";
 	private static final Pattern SMALL_RUSSIAN_START_LETTER_PATTERN = Pattern
 			.compile("^[а-я].*");
 	private static final String RU_LOCALE = "ru";
 	private static final String EN_LOCALE = "en";
 
+	private static final Map<String, String> propertyUri2code = Maps
+			.newHashMap();
+	private static final Map<String, String> classUri2code = Maps
+			.newLinkedHashMap();
+
 	private OntologyReportGenerator() {
 	}
 
 	public static XWPFDocument generate(OntModel model) {
+		initPropertyCodes(model);
+		initClassCodes(model);
 		XWPFDocument wordDocument = new XWPFDocument();
-		ExtendedIterator<OntClass> iterator = model.listNamedClasses();
-		List<OntClass> iteratorAsList = iterator.toList();
-		for (OntClass ontClass : iteratorAsList) {
+		Set<String> keys = classUri2code.keySet();
+		for (String ontClassUri : keys) {
+			OntClass ontClass = model.getOntClass(ontClassUri);
 			String rdfsLabel = ontClass.getLabel(RU_LOCALE) != null ? ontClass
 					.getLabel(RU_LOCALE) : ontClass.getLabel(EN_LOCALE);
 			rdfsLabel = rdfsLabel != null ? rdfsLabel : ontClass.getURI()
 					.substring(ontClass.getURI().indexOf("#") + 1);
-			String uri = ontClass.getURI();
 			String comment = ontClass.getComment(RU_LOCALE);
-			addURI(wordDocument, rdfsLabel);
+			addURI(wordDocument, ontClass);
 			addSuperclasses(wordDocument, ontClass);
+			addSubclasses(wordDocument, ontClass);
 			addComment(wordDocument, comment);
 			wordDocument.createParagraph().setSpacingAfter(10);
-			addSubclasses(wordDocument, ontClass);
 			addProperties(wordDocument, ontClass);
 		}
 		return wordDocument;
 	}
 
+	private static void initClassCodes(OntModel model) {
+		int i = 1;
+		Stack<OntClass> stack = new Stack<OntClass>();
+		stack.push(model.getOntClass(OWL_THING_URI));
+		while (!stack.isEmpty()) {
+			OntClass clazz = stack.pop();
+			if (!clazz.getURI().equals(OWL_THING_URI)
+					&& !classUri2code.containsKey(clazz.getURI())) {
+				classUri2code.put(clazz.getURI(), format("E%d", i));
+				i++;
+			}
+			List<OntClass> children = clazz.listSubClasses(true).toList();
+			Collections.sort(children, new OntClassByLabelComparatorDesc());
+			for (OntClass child : children) {
+				stack.push(child);
+			}
+		}
+	}
+
+	private static void initPropertyCodes(OntModel model) {
+		ExtendedIterator<ObjectProperty> propIt = model.listObjectProperties();
+		int i = 1;
+		while (propIt.hasNext()) {
+			ObjectProperty property = propIt.next();
+			propertyUri2code.put(property.getURI(), format("P%d", i));
+			i++;
+		}
+	}
+
 	private static void addProperties(XWPFDocument wordDocument,
 			OntClass ontClass) {
+		Set<OntClass> equivalentClasses = ontClass.listEquivalentClasses()
+				.toSet();
+		if (equivalentClasses.isEmpty())
+			return;
 		XWPFParagraph paragraph = wordDocument.createParagraph();
 		paragraph.setStyle("style0");
 		XWPFRun run = paragraph.createRun();
 		run.setText("Свойства: ");
-		Set<OntProperty> properties = ontClass.listDeclaredProperties(true)
-				.toSet();
-		for (OntProperty property : properties) {
+
+		for (OntClass equivalentClass : equivalentClasses) {
+			if (!equivalentClass.isRestriction())
+				continue;
+			AllValuesFromRestriction restriction = equivalentClass
+					.asRestriction().asAllValuesFromRestriction();
 			XWPFParagraph p = wordDocument.createParagraph();
 			p.setIndentationLeft(1440);
 			XWPFRun r = p.createRun();
-			r.setText(property.getLabel(RU_LOCALE));
+			OntProperty onProperty = restriction.getOnProperty();
+			OntClass toClass = restriction.getAllValuesFrom()
+					.as(OntClass.class);
+
+			r.setText(format("%s: %s", formatPropertyURI(onProperty),
+					formatURI(toClass)));
 			r.setItalic(true);
 		}
 		wordDocument.createParagraph().setIndentationLeft(1440);
+	}
+
+	private static String formatPropertyURI(OntProperty onProperty) {
+		return format("%s %s", propertyUri2code.get(onProperty.getURI()),
+				onProperty.getLabel(RU_LOCALE).replace("_", " "));
 	}
 
 	private static void addComment(XWPFDocument wordDocument, String comment) {
@@ -75,19 +136,19 @@ public final class OntologyReportGenerator {
 
 	private static void addNeighbourClasses(XWPFDocument wordDocument,
 			Set<OntClass> classes, String header) {
+		if (classes.isEmpty())
+			return;
 		XWPFParagraph paragraph = wordDocument.createParagraph();
 		paragraph.setStyle("style0");
 		XWPFRun run = paragraph.createRun();
 		run.setText(header);
 		StringBuilder sb = new StringBuilder();
-		Iterator<OntClass> it = classes.iterator();
+		List<OntClass> classList = new ArrayList<OntClass>(classes);
+		Collections.sort(classList, new OntClassByLabelComparatorAsc());
+		Iterator<OntClass> it = classList.iterator();
 		while (it.hasNext()) {
 			OntClass clazz = it.next();
-			String label = clazz.getLabel(RU_LOCALE) != null ? clazz
-					.getLabel(RU_LOCALE) : clazz.getLabel(EN_LOCALE);
-			label = label != null ? label : clazz.getURI().substring(
-					clazz.getURI().indexOf("#") + 1);
-			sb.append(formatURI(label));
+			sb.append(formatURI(clazz));
 			if (it.hasNext()) {
 				sb.append("; ");
 			}
@@ -102,8 +163,10 @@ public final class OntologyReportGenerator {
 
 	private static void addSubclasses(XWPFDocument wordDocument,
 			OntClass ontClass) {
-		addNeighbourClasses(wordDocument,
-				ontClass.listSubClasses(true).toSet(), "Подклассы: ");
+		Set<OntClass> subclasses = ontClass.listSubClasses(true).toSet();
+		if (!subclasses.isEmpty()) {
+			addNeighbourClasses(wordDocument, subclasses, "Подклассы: ");
+		}
 	}
 
 	private static void addSuperclasses(XWPFDocument wordDocument,
@@ -112,21 +175,56 @@ public final class OntologyReportGenerator {
 				.toSet(), "Суперклассы: ");
 	}
 
-	private static void addURI(XWPFDocument wordDocument, String uri) {
+	private static void addURI(XWPFDocument wordDocument, OntClass ontClass) {
 		XWPFParagraph paragraph = wordDocument.createParagraph();
 		paragraph.setSpacingBefore(10);
 		paragraph.setStyle("Heading 5");
 		XWPFRun run = paragraph.createRun();
-		run.setText(formatURI(uri));
+		run.setText(formatURI(ontClass));
 	}
 
-	private static String formatURI(String uri) {
-		if (SMALL_RUSSIAN_START_LETTER_PATTERN.matcher(uri).matches()) {
+	private static String formatURI(OntClass ontClass) {
+		String preparedLabel = prepareLabel(ontClass);
+		if (ontClass.getURI().equals(OWL_THING_URI))
+			return format("owl:%s", preparedLabel);
+		return format("%s %s", classUri2code.get(ontClass.getURI()),
+				preparedLabel);
+
+	}
+
+	private static String prepareLabel(OntClass ontClass) {
+		String rdfsLabel = ontClass.getLabel(RU_LOCALE) != null ? ontClass
+				.getLabel(RU_LOCALE) : ontClass.getLabel(EN_LOCALE);
+		rdfsLabel = rdfsLabel != null ? rdfsLabel : ontClass.getURI()
+				.substring(ontClass.getURI().indexOf("#") + 1);
+		if (SMALL_RUSSIAN_START_LETTER_PATTERN.matcher(rdfsLabel).matches()) {
 			StringBuilder sb = new StringBuilder();
-			sb.append(Character.toUpperCase(uri.charAt(0)));
-			sb.append(uri.substring(1));
-			uri = sb.toString();
+			sb.append(Character.toUpperCase(rdfsLabel.charAt(0)));
+			sb.append(rdfsLabel.substring(1));
+			rdfsLabel = sb.toString();
 		}
-		return uri.replace("_", " ");
+
+		String preparedLabel = rdfsLabel.replace("_", " ");
+		return preparedLabel;
+	}
+
+	private static class OntClassByLabelComparatorDesc implements
+			Comparator<OntClass> {
+
+		@Override
+		public int compare(OntClass first, OntClass second) {
+			return prepareLabel(second).compareTo(prepareLabel(first));
+		}
+
+	}
+
+	private static class OntClassByLabelComparatorAsc implements
+			Comparator<OntClass> {
+
+		@Override
+		public int compare(OntClass first, OntClass second) {
+			return prepareLabel(first).compareTo(prepareLabel(second));
+		}
+
 	}
 }
