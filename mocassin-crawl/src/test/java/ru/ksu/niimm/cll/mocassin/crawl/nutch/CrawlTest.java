4941b7e93646625be6bb6cb09f5ce3f51d579778
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2012-04-30 15:20:35 +0000

    distinguished using nutch under the test settings and on production; added a dependency for Virtuoso JDBC [Issue 90]

---
+++ mocassin-crawl/src/test/java/ru/ksu/niimm/cll/mocassin/crawl/nutch/CrawlTest.java
@@ -0,0 +1,14 @@
+package ru.ksu.niimm.cll.mocassin.crawl.nutch;
+
+import org.apache.nutch.crawl.Crawl;
+import org.junit.Test;
+
+public class CrawlTest {
+
+	@Test
+	public void testParse() throws Exception {
+		String[] args = { "/opt/mocassin/test-urls", "-depth", "1", "-dir",
+				"/opt/mocassin/test-crawl" };
+		Crawl.main(args);
+	}
+}
