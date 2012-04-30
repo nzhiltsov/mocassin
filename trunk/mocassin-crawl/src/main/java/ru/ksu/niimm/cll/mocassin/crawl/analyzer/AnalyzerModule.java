33fcfda194e314d19b046a1fc27751214e664c6a
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2012-04-30 16:15:40 +0000

    deleted unused classes [Issue 86]

--- trunk/mocassin-crawl/src/main/java/ru/ksu/niimm/cll/mocassin/crawl/analyzer/AnalyzerModule.java
+++ trunk/mocassin-crawl/src/main/java/ru/ksu/niimm/cll/mocassin/crawl/analyzer/AnalyzerModule.java
@@ -8,9 +8,7 @@ import java.util.Properties;
 import ru.ksu.niimm.cll.mocassin.crawl.analyzer.impl.GateReferenceSearcher;
 import ru.ksu.niimm.cll.mocassin.crawl.analyzer.impl.ReferenceStatementGeneratorImpl;
 import ru.ksu.niimm.cll.mocassin.crawl.analyzer.importance.ImportantElementService;
-import ru.ksu.niimm.cll.mocassin.crawl.analyzer.importance.ImportantNodeService;
 import ru.ksu.niimm.cll.mocassin.crawl.analyzer.importance.impl.PageRankElementService;
-import ru.ksu.niimm.cll.mocassin.crawl.analyzer.importance.impl.PageRankNodeService;
 import ru.ksu.niimm.cll.mocassin.crawl.analyzer.location.ReferenceElementLocationAnalyzer;
 import ru.ksu.niimm.cll.mocassin.crawl.analyzer.location.impl.ReferenceElementLocationAnalyzerImpl;
 import ru.ksu.niimm.cll.mocassin.crawl.analyzer.lsa.LSIPropertiesLoader;
@@ -60,7 +58,6 @@ public class AnalyzerModule extends AbstractModule {
 		bind(NameMatcherPropertiesLoader.class).to(
 				NameMatcherPropertiesLoaderImpl.class);
 
-		bind(ImportantNodeService.class).to(PageRankNodeService.class);
 		bind(ImportantElementService.class).to(PageRankElementService.class);
 
 		bind(LatentSemanticIndexer.class).to(LatentSemanticIndexerImpl.class);
--- trunk/mocassin-crawl/src/main/java/ru/ksu/niimm/cll/mocassin/crawl/analyzer/importance/ImportantNodeService.java
+++
@@ -1,8 +0,0 @@
-package ru.ksu.niimm.cll.mocassin.crawl.analyzer.importance;
-
-import ru.ksu.niimm.cll.mocassin.crawl.parser.latex.Edge;
-import ru.ksu.niimm.cll.mocassin.crawl.parser.latex.Node;
-
-
-public interface ImportantNodeService extends ImportanceService<Node, Edge> {
-}
