e8a7b46db6e59b2e9ec7803e7fb206abb0f9a628
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2012-05-01 11:12:55 +0000

    added copyright headers [Issue 86]

--- trunk/mocassin-crawl/src/main/java/ru/ksu/niimm/cll/mocassin/crawl/analyzer/relation/HasConsequenceRelationAnalyzer.java
+++ trunk/mocassin-crawl/src/main/java/ru/ksu/niimm/cll/mocassin/crawl/analyzer/relation/HasConsequenceRelationAnalyzer.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2012 Nikita Zhiltsov.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Nikita Zhiltsov - initial API and implementation
+ *     Azat Khasanshin - implementation
+ ******************************************************************************/
 package ru.ksu.niimm.cll.mocassin.crawl.analyzer.relation;
 
 import ru.ksu.niimm.cll.mocassin.crawl.parser.gate.ParsedDocument;
