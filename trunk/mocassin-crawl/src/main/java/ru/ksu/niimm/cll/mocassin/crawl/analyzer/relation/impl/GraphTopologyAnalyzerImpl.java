fb7ba22f44f97a5971d13b650e47ec5f9987b5ee
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2012-05-02 16:36:30 +0000

    added the basics for extraction of preferential attachment scores [Issue 95]

--- trunk/mocassin-crawl/src/main/java/ru/ksu/niimm/cll/mocassin/crawl/analyzer/relation/impl/GraphTopologyAnalyzerImpl.java
+++ trunk/mocassin-crawl/src/main/java/ru/ksu/niimm/cll/mocassin/crawl/analyzer/relation/impl/GraphTopologyAnalyzerImpl.java
@@ -12,6 +12,7 @@
 package ru.ksu.niimm.cll.mocassin.crawl.analyzer.relation.impl;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import edu.uci.ics.jung.graph.Graph;
@@ -21,32 +22,39 @@ import ru.ksu.niimm.cll.mocassin.crawl.analyzer.relation.RelationFeatureInfo;
 import ru.ksu.niimm.cll.mocassin.crawl.parser.gate.Reference;
 import ru.ksu.niimm.cll.mocassin.crawl.parser.gate.StructuralElement;
 
+import static ru.ksu.niimm.cll.mocassin.util.GraphMetricUtils.*;
+
 public class GraphTopologyAnalyzerImpl implements GraphTopologyAnalyzer {
 
-	@Override
-	public List<RelationFeatureInfo> extractCandidateFeatures(
-			Graph<StructuralElement, Reference> graph) {
-        ArrayList<RelationFeatureInfo> result = new ArrayList<RelationFeatureInfo>();
+    @Override
+    public List<RelationFeatureInfo> extractCandidateFeatures(
+	    Graph<StructuralElement, Reference> graph) {
+	ArrayList<RelationFeatureInfo> result = new ArrayList<RelationFeatureInfo>();
 
-        ArrayList<StructuralElement> elements = new ArrayList<StructuralElement>(graph.getVertices());
-        int size = graph.getVertices().size();
-        for (int i = 0; i < size - 1; i++) {
-            ArrayList<StructuralElement> iNeighbors =
-                    new ArrayList<StructuralElement>(graph.getNeighbors(elements.get(i)));
-            for (int j = i + 1; j < size; j++) {
-                ArrayList<StructuralElement> jNeighbors =
-                        new ArrayList<StructuralElement>(graph.getNeighbors(elements.get(j)));
+	ArrayList<StructuralElement> elements = new ArrayList<StructuralElement>(
+		graph.getVertices());
+	int size = graph.getVertices().size();
+	for (int i = 0; i < size - 1; i++) {
+	    ArrayList<StructuralElement> iNeighbors = new ArrayList<StructuralElement>(
+		    graph.getNeighbors(elements.get(i)));
+	    for (int j = i + 1; j < size; j++) {
+		ArrayList<StructuralElement> jNeighbors = new ArrayList<StructuralElement>(
+			graph.getNeighbors(elements.get(j)));
 
-                int intersection = CollectionUtils.intersection(iNeighbors, jNeighbors).size();
-                int union = CollectionUtils.union(iNeighbors, jNeighbors).size();
+		float jaccard = computeNeighborJaccard(iNeighbors, jNeighbors);
 
-                float jaccard = (float) intersection / union;
+		int preferentialAttachmentScore = computePreferentialAttachmentScore(
+			iNeighbors, jNeighbors);
 
-                RelationFeatureInfo info = new RelationFeatureInfo(elements.get(i), elements.get(j), jaccard);
-                result.add(info);
-            }
-        }
-		return result;
+		RelationFeatureInfo info = new RelationFeatureInfo.Builder(
+			elements.get(i), elements.get(j))
+			.neigborJaccard(jaccard)
+			.preferentialAttachmentScore(
+				preferentialAttachmentScore).build();
+		result.add(info);
+	    }
 	}
+	return result;
+    }
 
 }
