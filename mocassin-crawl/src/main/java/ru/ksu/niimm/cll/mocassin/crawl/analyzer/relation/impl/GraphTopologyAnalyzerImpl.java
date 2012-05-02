5992f92c6515cabe2a4922966df67eb681e2cf0c
lordofbazuks <lordofbazuks@gmail.com>
2012-05-02 14:12:19 +0000

    Issue 94

--- mocassin-crawl/src/main/java/ru/ksu/niimm/cll/mocassin/crawl/analyzer/relation/impl/GraphTopologyAnalyzerImpl.java
+++ mocassin-crawl/src/main/java/ru/ksu/niimm/cll/mocassin/crawl/analyzer/relation/impl/GraphTopologyAnalyzerImpl.java
@@ -11,9 +11,11 @@
  ******************************************************************************/
 package ru.ksu.niimm.cll.mocassin.crawl.analyzer.relation.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.uci.ics.jung.graph.Graph;
+import org.apache.commons.collections.CollectionUtils;
 import ru.ksu.niimm.cll.mocassin.crawl.analyzer.relation.GraphTopologyAnalyzer;
 import ru.ksu.niimm.cll.mocassin.crawl.analyzer.relation.RelationFeatureInfo;
 import ru.ksu.niimm.cll.mocassin.crawl.parser.gate.Reference;
@@ -24,7 +26,27 @@ public class GraphTopologyAnalyzerImpl implements GraphTopologyAnalyzer {
 	@Override
 	public List<RelationFeatureInfo> extractCandidateFeatures(
 			Graph<StructuralElement, Reference> graph) {
-		throw new UnsupportedOperationException("not implemented yet");
+        ArrayList<RelationFeatureInfo> result = new ArrayList<RelationFeatureInfo>();
+
+        ArrayList<StructuralElement> elements = new ArrayList<StructuralElement>(graph.getVertices());
+        int size = graph.getVertices().size();
+        for (int i = 0; i < size - 1; i++) {
+            ArrayList<StructuralElement> iNeighbors =
+                    new ArrayList<StructuralElement>(graph.getNeighbors(elements.get(i)));
+            for (int j = i + 1; j < size; j++) {
+                ArrayList<StructuralElement> jNeighbors =
+                        new ArrayList<StructuralElement>(graph.getNeighbors(elements.get(j)));
+
+                int intersection = CollectionUtils.intersection(iNeighbors, jNeighbors).size();
+                int union = CollectionUtils.union(iNeighbors, jNeighbors).size();
+
+                float jaccard = (float) intersection / union;
+
+                RelationFeatureInfo info = new RelationFeatureInfo(elements.get(i), elements.get(j), jaccard);
+                result.add(info);
+            }
+        }
+		return result;
 	}
 
 }
