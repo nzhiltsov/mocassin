91b37b6db6d78b82cc8528ba324d6bc9c92325f3
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2012-05-06 09:21:21 +0000

    fixed the provider [Issue 86]

--- trunk/mocassin-crawl/src/main/java/ru/ksu/niimm/cll/mocassin/crawl/parser/gate/AnnieControllerProviderImpl.java
+++ trunk/mocassin-crawl/src/main/java/ru/ksu/niimm/cll/mocassin/crawl/parser/gate/AnnieControllerProviderImpl.java
@@ -11,38 +11,44 @@
  ******************************************************************************/
 package ru.ksu.niimm.cll.mocassin.crawl.parser.gate;
 
+import gate.Factory;
 import gate.Gate;
 import gate.creole.ANNIEConstants;
+import gate.creole.ResourceInstantiationException;
 import gate.creole.SerialAnalyserController;
+import gate.persist.PersistenceException;
 import gate.util.persistence.PersistenceManager;
 
 import java.io.File;
+import java.io.IOException;
 
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 
 public class AnnieControllerProviderImpl implements
-		AnnieControllerProvider<SerialAnalyserController> {
-	private final String CONFIG_FILE_NAME;
+	AnnieControllerProvider<SerialAnalyserController> {
+    private final String CONFIG_FILE_NAME;
 
-	@Inject
-	public AnnieControllerProviderImpl(
-			@Named("gate.loaded.plugin.config") String configName) {
-		CONFIG_FILE_NAME = configName;
-	}
+    private final SerialAnalyserController annie;
 
-	@Override
-	public SerialAnalyserController get()
-			throws AnnieControllerCreationException {
-		SerialAnalyserController annie;
-		try {
-			annie = (SerialAnalyserController) PersistenceManager
-					.loadObjectFromFile(new File(new File(
-							Gate.getPluginsHome(), ANNIEConstants.PLUGIN_DIR),
-							CONFIG_FILE_NAME));
-		} catch (Exception e) {
-			throw new AnnieControllerCreationException(e);
-		}
-		return annie;
+    @Inject
+    public AnnieControllerProviderImpl(
+	    @Named("gate.loaded.plugin.config") String configName)
+	    throws PersistenceException, ResourceInstantiationException,
+	    IOException {
+	CONFIG_FILE_NAME = configName;
+	annie = (SerialAnalyserController) PersistenceManager
+		.loadObjectFromFile(new File(new File(Gate.getPluginsHome(),
+			ANNIEConstants.PLUGIN_DIR), CONFIG_FILE_NAME));
+    }
+
+    @Override
+    public SerialAnalyserController get()
+	    throws AnnieControllerCreationException {
+	try {
+	    return (SerialAnalyserController) Factory.duplicate(annie);
+	} catch (ResourceInstantiationException e) {
+	    throw new AnnieControllerCreationException(e);
 	}
+    }
 }
