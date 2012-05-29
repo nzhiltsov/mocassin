78c180de9c1c18941449801c5e78b9eb32040040
nikita.zhiltsov <nikita.zhiltsov@8a933764-0218-11df-adb7-1be7f7e3f636>
2012-05-29 17:13:44 +0000

    added extraction of containing terms, formulas and variables per document segment

--- trunk/mocassin-crawl/src/main/java/ru/ksu/niimm/cll/mocassin/crawl/analyzer/impl/StructuralElementImpl.java
+++ trunk/mocassin-crawl/src/main/java/ru/ksu/niimm/cll/mocassin/crawl/analyzer/impl/StructuralElementImpl.java
@@ -21,110 +21,46 @@ import ru.ksu.niimm.cll.mocassin.rdf.ontology.MocassinOntologyClasses;
 import com.google.common.base.Predicate;
 
 public class StructuralElementImpl implements StructuralElement {
-	/**
-	 * added for compatibility; for genuine uniqueness see
-	 * {@link #equals(Object)}, which is used URI field value
-	 */
+    /**
+     * added for compatibility; for genuine uniqueness see
+     * {@link #equals(Object)}, which uses URI field value
+     */
+    private final int id;
+    private final String uri;
+    private final long start;
+    private final long end;
+    private final String name;
+    private List<String> labels;
+    private final String title;
+    private List<String> contents = new LinkedList<String>();
+    private final List<Term> terms = new ArrayList<Term>();
+
+    private int startPageNumber;
+    private MocassinOntologyClasses predictedClass;
+    private int latexStartLine;
+    private int latexEndLine;
+
+    public static class Builder {
 	private final int id;
 	private final String uri;
-	private final long start;
-	private final long end;
-	private final String name;
-	private List<String> labels;
-	private final String title;
-	private List<Token> contents = new LinkedList<Token>();
+	private long start;
+	private long end;
+	private String name;
+	private String title;
 
-	private int startPageNumber;
-	private MocassinOntologyClasses predictedClass;
-	private int latexStartLine;
-	private int latexEndLine;
-
-	public static class Builder {
-		private final int id;
-		private final String uri;
-		private long start;
-		private long end;
-		private String name;
-		private String title;
-
-		public Builder(int id, String uri) {
-			this.id = id;
-			this.uri = uri;
-		}
-
-		public Builder start(long start) {
-			this.start = start;
-			return this;
-		}
-
-		public Builder end(long end) {
-			this.end = end;
-			return this;
-		}
-
-		public Builder name(String name) {
-			this.name = name;
-			return this;
-		}
-
-		public Builder title(String title) {
-			this.title = title;
-			return this;
-		}
-
-		public StructuralElement build() {
-			return new StructuralElementImpl(this);
-		}
-
+	public Builder(int id, String uri) {
+	    this.id = id;
+	    this.uri = uri;
 	}
 
-	public StructuralElementImpl(Builder builder) {
-		this.id = builder.id;
-		this.uri = builder.uri;
-		this.start = builder.start;
-		this.end = builder.end;
-		this.name = builder.name;
-		this.title = builder.title;
+	public Builder start(long start) {
+	    this.start = start;
+	    return this;
 	}
 
-	public List<String> getLabels() {
-		return labels;
-	}
-
-	public void setLabels(List<String> labels) {
-		this.labels = labels;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public String getUri() {
-		return uri;
-	}
-
-	public long getGateStartOffset() {
-		return start;
-	}
-
-	public long getGateEndOffset() {
-		return end;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public MocassinOntologyClasses getPredictedClass() {
-		return predictedClass;
-	}
-
-	public void setPredictedClass(MocassinOntologyClasses predictedClass) {
-		this.predictedClass = predictedClass;
+	public Builder end(long end) {
+	    this.end = end;
+	    return this;
 	}
 
 	public List<String> getContents() {
@@ -147,132 +83,205 @@ public class StructuralElementImpl implements StructuralElement {
 		Collections.addAll(this.contents, contents);
 	}
 
-	public int getStartPageNumber() {
-		return startPageNumber;
+	public Builder title(String title) {
+	    this.title = title;
+	    return this;
 	}
 
-	public void setStartPageNumber(int startPageNumber) {
-		this.startPageNumber = startPageNumber;
+	public StructuralElement build() {
+	    return new StructuralElementImpl(this);
 	}
 
-	public int getLatexStartLine() {
-		return latexStartLine;
+    }
+
+    public StructuralElementImpl(Builder builder) {
+	this.id = builder.id;
+	this.uri = builder.uri;
+	this.start = builder.start;
+	this.end = builder.end;
+	this.name = builder.name;
+	this.title = builder.title;
+    }
+
+    public List<String> getLabels() {
+	return labels;
+    }
+
+    public void setLabels(List<String> labels) {
+	this.labels = labels;
+    }
+
+    public String getTitle() {
+	return title;
+    }
+
+    public int getId() {
+	return id;
+    }
+
+    public String getUri() {
+	return uri;
+    }
+
+    public long getGateStartOffset() {
+	return start;
+    }
+
+    public long getGateEndOffset() {
+	return end;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public MocassinOntologyClasses getPredictedClass() {
+	return predictedClass;
+    }
+
+    public void setPredictedClass(MocassinOntologyClasses predictedClass) {
+	this.predictedClass = predictedClass;
+    }
+
+    public List<String> getContents() {
+	return contents;
+    }
+
+    public void setContents(String... contents) {
+	Collections.addAll(this.contents, contents);
+    }
+
+    public void setTerms(Term... terms) {
+	Collections.addAll(this.terms, terms);
+    }
+
+    public int getStartPageNumber() {
+	return startPageNumber;
+    }
+
+    public void setStartPageNumber(int startPageNumber) {
+	this.startPageNumber = startPageNumber;
+    }
+
+    public int getLatexStartLine() {
+	return latexStartLine;
+    }
+
+    public void setLatexStartLine(int latexStartLine) {
+	this.latexStartLine = latexStartLine;
+    }
+
+    public int getLatexEndLine() {
+	return latexEndLine;
+    }
+
+    public void setLatexEndLine(int latexEndLine) {
+	this.latexEndLine = latexEndLine;
+    }
+
+    @Override
+    public String toString() {
+	return String.format("%s; %s; %s %d", uri, predictedClass, name,
+		latexStartLine);
+    }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((uri == null) ? 0 : uri.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	StructuralElementImpl other = (StructuralElementImpl) obj;
+	if (uri == null) {
+	    if (other.uri != null)
+		return false;
+	} else if (!uri.equals(other.uri))
+	    return false;
+	return true;
+    }
+
+    @SuppressWarnings("serial")
+    public static class DescPositionComparator implements Serializable,
+	    Comparator<StructuralElement> {
+
+	@Override
+	public int compare(StructuralElement first, StructuralElement second) {
+	    if (first.getGateStartOffset() < second.getGateStartOffset())
+		return 1;
+	    if (first.getGateStartOffset() > second.getGateStartOffset())
+		return -1;
+	    if (first.getGateEndOffset() - first.getGateStartOffset() > second
+		    .getGateEndOffset() - second.getGateStartOffset())
+		return 1;
+	    if (first.getGateEndOffset() - first.getGateStartOffset() < second
+		    .getGateEndOffset() - second.getGateStartOffset())
+		return -1;
+	    return 0;
+	}
+    }
+
+    @SuppressWarnings("serial")
+    public static class IdComparator implements Serializable,
+	    Comparator<StructuralElement> {
+
+	@Override
+	public int compare(StructuralElement first, StructuralElement second) {
+	    Integer firstId = first.getId();
+	    Integer secondId = second.getId();
+	    return firstId.compareTo(secondId);
 	}
 
-	public void setLatexStartLine(int latexStartLine) {
-		this.latexStartLine = latexStartLine;
-	}
+    }
 
-	public int getLatexEndLine() {
-		return latexEndLine;
-	}
+    public static class TypePredicate implements Predicate<StructuralElement> {
+	private final MocassinOntologyClasses type;
 
-	public void setLatexEndLine(int latexEndLine) {
-		this.latexEndLine = latexEndLine;
+	public TypePredicate(MocassinOntologyClasses type) {
+	    this.type = type;
 	}
 
 	@Override
-	public String toString() {
-		return String.format("%s; %s; %s %d", uri, predictedClass, name, latexStartLine);
+	public boolean apply(StructuralElement elem) {
+	    return this.type == elem.getPredictedClass();
+	}
+
+    }
+
+    public static class TypeFilterPredicate implements
+	    Predicate<StructuralElement> {
+	private final List<MocassinOntologyClasses> filterTypes;
+
+	public TypeFilterPredicate(MocassinOntologyClasses[] filterTypes) {
+	    this.filterTypes = Arrays.asList(filterTypes);
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((uri == null) ? 0 : uri.hashCode());
-		return result;
+	public boolean apply(StructuralElement element) {
+	    MocassinOntologyClasses elementType = element.getPredictedClass();
+	    return this.filterTypes.contains(elementType);
+	}
+    }
+
+    public static class IdPredicate implements Predicate<StructuralElement> {
+	private final int id;
+
+	public IdPredicate(int id) {
+	    this.id = id;
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		StructuralElementImpl other = (StructuralElementImpl) obj;
-		if (uri == null) {
-			if (other.uri != null)
-				return false;
-		} else if (!uri.equals(other.uri))
-			return false;
-		return true;
+	public boolean apply(StructuralElement input) {
+	    return id == input.getId();
 	}
-
-	@SuppressWarnings("serial")
-	public static class DescPositionComparator implements Serializable,
-			Comparator<StructuralElement> {
-
-		@Override
-		public int compare(StructuralElement first, StructuralElement second) {
-			if (first.getGateStartOffset() < second.getGateStartOffset())
-				return 1;
-			if (first.getGateStartOffset() > second.getGateStartOffset())
-				return -1;
-			if (first.getGateEndOffset() - first.getGateStartOffset() > second
-					.getGateEndOffset() - second.getGateStartOffset())
-				return 1;
-			if (first.getGateEndOffset() - first.getGateStartOffset() < second
-					.getGateEndOffset() - second.getGateStartOffset())
-				return -1;
-			return 0;
-		}
-	}
-
-	@SuppressWarnings("serial")
-	public static class IdComparator implements Serializable,
-			Comparator<StructuralElement> {
-
-		@Override
-		public int compare(StructuralElement first, StructuralElement second) {
-			Integer firstId = first.getId();
-			Integer secondId = second.getId();
-			return firstId.compareTo(secondId);
-		}
-
-	}
-
-	public static class TypePredicate implements Predicate<StructuralElement> {
-		private final MocassinOntologyClasses type;
-
-		public TypePredicate(MocassinOntologyClasses type) {
-			this.type = type;
-		}
-
-		@Override
-		public boolean apply(StructuralElement elem) {
-			return this.type == elem.getPredictedClass();
-		}
-
-	}
-
-	public static class TypeFilterPredicate implements
-			Predicate<StructuralElement> {
-		private final List<MocassinOntologyClasses> filterTypes;
-
-		public TypeFilterPredicate(MocassinOntologyClasses[] filterTypes) {
-			this.filterTypes = Arrays.asList(filterTypes);
-		}
-
-		@Override
-		public boolean apply(StructuralElement element) {
-			MocassinOntologyClasses elementType = element.getPredictedClass();
-			return this.filterTypes.contains(elementType);
-		}
-	}
-
-	public static class IdPredicate implements Predicate<StructuralElement> {
-		private final int id;
-
-		public IdPredicate(int id) {
-			this.id = id;
-		}
-
-		@Override
-		public boolean apply(StructuralElement input) {
-			return id == input.getId();
-		}
-	}
+    }
 }
